6942c9fd4242a8e62c25e3f5f9aba3115385cce7
Jannik Buschke <jbu.extern@silvermonkey.net>
2019-01-15 22:51:16 +0100

    wip

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,111 @@
+import { Link } from "@reach/router";
+import { Layout, Menu } from "antd";
+import * as React from "react";
+
+const { Header, Content, Sider } = Layout;
+
+interface NavigationLink {
+  displayName: string;
+  icon?: string;
+  to: string;
+  kind: "LINK";
+}
+
+interface Custom {
+  component: React.ReactNode;
+  kind: "CUSTOM";
+}
+
+type NavigationItem = NavigationLink | Custom;
+
+export const NavigationItems = ({ items }: { items: NavigationItem[] }) => (
+  <div>
+    {items.map(item => {
+      switch (item.kind) {
+        case "LINK":
+          return (
+            <Menu.Item key={item.to}>
+              <div>asd</div>
+              <Link to={item.to}>{item.displayName}</Link>
+            </Menu.Item>
+          );
+        case "CUSTOM":
+          return <div key={Math.random()}>asd</div>;
+        default:
+          return <div>null</div>;
+      }
+    })}
+  </div>
+);
+
+interface Props {
+  header?: NavigationItem[];
+  sideBarItems?: NavigationItem[];
+  children: any;
+}
+
+export const FlatMenu = (props: { items: NavigationItem[] }) => (
+  <Menu mode="inline" style={{ height: "100%", borderRight: 0 }}>
+    {props.items.map(item => {
+      switch (item.kind) {
+        case "LINK":
+          return (
+            <Menu.Item key={item.to}>
+              <Link to={item.to}>{item.displayName}</Link>
+            </Menu.Item>
+          );
+        case "CUSTOM":
+          return item.component;
+        default:
+          return <div>null</div>;
+      }
+    })}
+  </Menu>
+);
+
+export const ApplicationLayout = ({
+  header,
+  sideBarItems,
+  children
+}: Props) => (
+  <Layout style={{ minHeight: "100vh" }}>
+    {header && (
+      <Header style={{ height: "auto" }}>
+        <Menu theme="dark" mode="horizontal">
+          {header.map((item: NavigationLink) => {
+            return (
+              <Menu.Item key={item.to}>
+                <Link to={item.to}>{item.displayName}</Link>
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+      </Header>
+    )}
+    <Layout>
+      {sideBarItems && (
+        <Sider
+          width={200}
+          style={{
+            background: "#fff"
+          }}
+        >
+          <FlatMenu items={sideBarItems} />
+        </Sider>
+      )}
+      <Layout style={{ padding: "24px 24px 24px" }}>
+        <Content
+          style={{
+            height: "1fr",
+            background: "#fff",
+            padding: 24,
+            margin: 0,
+            minHeight: 280
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
+);
--- src/types.tsx
+++ src/types.tsx
@@ -1,5 +1,1 @@
-export * from "./Notification";
-export * from "./Pages";
-export * from "./Pages/types";
-export * from "./Formik/index";
-export * from "./Api/index";
+export * from "./index";
