55a39b50754d24c3bb28e8c892854244c0e77f8a
Jannik Buschke <jannik.buschke@gmail.com>
2019-03-30 19:29:26 +0100

    use a type for part of the union props (instead of an interfacewq)

--- src/Formik/NavigateButton.tsx
+++ src/Formik/NavigateButton.tsx
@@ -3,11 +3,9 @@ import * as React from "react";
 import { Link } from "@reach/router";
 import { ButtonProps } from "antd/lib/button";
 
-export interface INavigateButtonProps {
-  to: string;
-}
+type Props = { to: string } & ButtonProps;
 
-export const NavigateButton = (props: INavigateButtonProps & ButtonProps) => (
+export const NavigateButton = (props: Props) => (
   <Button type="primary" {...props}>
     <Link to={props.to}>new</Link>
   </Button>
