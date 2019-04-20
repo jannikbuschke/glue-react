a2c057d7705e8773e6c1aeb44a29deb3260b617c
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-20 20:13:18 +0200

    add onSuccess and onError handlers and show a delayed loading indicator for EntityActionButton

--- src/Formik/EntityActionButton.tsx
+++ src/Formik/EntityActionButton.tsx
@@ -1,4 +1,4 @@
-import { Button, message } from "antd";
+import { Button, message, Spin } from "antd";
 import { Field, FormikProps } from "formik";
 import * as React from "react";
 import { ButtonProps } from "antd/lib/button";
@@ -6,33 +6,55 @@ import { ButtonProps } from "antd/lib/button";
 type Props = {
   payload: any;
   path: string;
+  onSuccess?: (value?: any) => void;
+  onError?: (e: any) => void;
 };
 
-export const EntityActionButton = (props: ButtonProps & Props) => {
+export const EntityActionButton = ({
+  path,
+  payload,
+  onSuccess,
+  onError,
+  ...props
+}: ButtonProps & Props) => {
   const [loading, setLoading] = React.useState(false);
   return (
-    <Field>
-      {({ form }: { field: any; form: FormikProps<any> }) => (
-        <Button
-          onClick={async (e: any) => {
-            setLoading(true);
-            try {
-              await fetch(props.path, {
-                method: "POST",
-                body: JSON.stringify(props.payload),
-                headers: { "content-type": "application/json" }
-              });
-            } catch (E) {
-              message.error(E.toString());
-            } finally {
-              setLoading(false);
-            }
-          }}
-          loading={form.isSubmitting || loading}
-          disabled={form.dirty}
-          {...props}
-        />
-      )}
-    </Field>
+    <Spin delay={750} spinning={loading}>
+      <Field>
+        {({ form }: { field: any; form: FormikProps<any> }) => (
+          <Button
+            onClick={async (e: any) => {
+              setLoading(true);
+              try {
+                const response = await fetch(path, {
+                  method: "POST",
+                  body: JSON.stringify(payload),
+                  headers: { "content-type": "application/json" }
+                });
+                if (onSuccess && response.ok) {
+                  if (response.status === 201) {
+                    const value = await response.json();
+                    onSuccess(value);
+                  } else {
+                    onSuccess();
+                  }
+                }
+              } catch (E) {
+                if (onError) {
+                  onError(E.toString());
+                } else {
+                  message.error(E.toString());
+                }
+              } finally {
+                setLoading(false);
+              }
+            }}
+            // loading={form.isSubmitting || loading}
+            disabled={form.dirty || loading}
+            {...props}
+          />
+        )}
+      </Field>
+    </Spin>
   );
 };
