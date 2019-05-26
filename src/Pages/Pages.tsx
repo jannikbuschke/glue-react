a550d8e95c58a3ca0222a5d1e4a5ddff533dccfe
Jannik Buschke <jannik.buschke@gmail.com>
2019-05-26 17:16:49 +0200

    remove Pages div container (and format)

--- src/Pages/Pages.tsx
+++ src/Pages/Pages.tsx
@@ -11,32 +11,31 @@ export const HorizontalSplit = ({
   templateColumns = "1fr 1fr",
   children,
 }: Props) => (
-  <div
-    style={{
-      display: "grid",
-      gridGap: "20px",
-      gridTemplateColumns: templateColumns,
-    }}
-  >
-    {children}
-  </div>
-)
+    <div
+      style={{
+        display: "grid",
+        gridGap: "20px",
+        gridTemplateColumns: templateColumns,
+      }}
+    >
+      {children}
+    </div>
+  )
 
-export const MasterDetailView = ({ item }: { item: IEntityItem }) => (
-  <div>
-    <Router primary={false}>
-      <item.list path={`${item.path}/*`} />
-    </Router>
-    <Router primary={false}>
-      <item.new path={`${item.path}/new`} />
-      {item.create && <item.create path={`${item.path}/create`} />}
-      {item.batchCreate && (
-        <item.batchCreate path={`${item.path}/batch-create`} />
-      )}
-      <item.detail path={`${item.path}/:id`} />
-    </Router>
-  </div>
-)
+export const MasterDetailView = ({ item }: { item: IEntityItem }) => (<>
+  <Router primary={false}>
+    <item.list path={`${item.path}/*`} />
+  </Router>
+  <Router primary={false}>
+    <item.new path={`${item.path}/new`} />
+    {item.create && <item.create path={`${item.path}/create`} />}
+    {item.batchCreate && (
+      <item.batchCreate path={`${item.path}/batch-create`} />
+    )}
+    <item.detail path={`${item.path}/:id`} />
+  </Router>
+</>
+);
 
 const MasterDetailContainer = (props: any) => (
   <div
