9fc86b4a18ecc5f47003b98becb4697a89081d87
Jannik Buschke <jannik.buschke@gmail.com>
2019-07-07 20:53:57 +0200

    add new Form component and useActions

---
+++ src/Forms/Form.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { Formik, FormikProps } from "formik";
+import { Spin, Alert, message } from "antd";
+import { FetchContext } from "@jbuschke/react-fetch-context"
+import { useActions } from './useActions';
+
+interface FormProps {
+    entityName: string;
+    actionName: string;
+    initialValues: any;
+    loading?: boolean;
+    error?: string;
+    children: any;
+    onSuccessfulSubmit?: (response: Response) => void;
+    addHeaders?: () => Promise<HeadersInit>;
+    validateOnChange?: boolean;
+    apiVersion?: string;
+}
+
+export const Form = ({
+    initialValues,
+    loading,
+    error,
+    children,
+    entityName,
+    actionName,
+    onSuccessfulSubmit,
+    addHeaders,
+    validateOnChange,
+    apiVersion = "1.0"
+}: FormProps) => {
+
+    const { submit, validate } = useActions(`/api/${entityName}/${actionName}?api-version=${apiVersion}`)
+
+    return (
+        <Formik
+            enableReinitialize={true}
+            initialValues={initialValues}
+            validate={async values => {
+                await validate(values)
+            }}
+            onSubmit={async (values, actions) => {
+                actions.setSubmitting(true);
+                const response = await submit(values)
+                actions.setSubmitting(false);
+                if (response.ok) {
+                    onSuccessfulSubmit && onSuccessfulSubmit(response);
+                } else {
+                    message.error(response.statusText);
+                }
+            }}
+            validateOnBlur={true}
+            validateOnChange={validateOnChange !== undefined ? validateOnChange : true}
+            render={(formProps: FormikProps<any>) => (
+                <Spin spinning={loading === true} delay={250}>
+                    {error ? (
+                        <div>
+                            <Alert
+                                message={error}
+                                showIcon={false}
+                                type="error"
+                                closable={true}
+                                banner={true}
+                                style={{ marginBottom: 10 }}
+                            />
+                            <div style={{ pointerEvents: "none", opacity: 0.6 }}>
+
+                                {children(formProps)}
+                            </div>
+                        </div>
+                    ) : (
+                            children(formProps)
+                        )}
+                </Spin>
+            )}
+        />
+    );
+}
