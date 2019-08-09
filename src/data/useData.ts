6b1004dbd575b14e12ad58461434d7589c9ae029
Jannik Buschke <jannik.buschke@gmail.com>
2019-08-09 14:10:30 +0200

    use formik-antd 1.2.2

--- src/data/useData.ts
+++ src/data/useData.ts
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { useEffect, useState } from "react"
 import { FetchContext } from "@jbuschke/react-fetch-context"
-
+// use
 export function useData<T>(
   uri: string,
   placeholder?: T,
