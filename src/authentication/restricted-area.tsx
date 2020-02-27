83a0c9c838611bd62c7b0e3a14679a46e0f09bb5
Jannik Buschke <jannik.buschke@gmail.com>
2020-02-27 17:39:16 +0100

    useSubmit: expose error

--- src/authentication/restricted-area.tsx
+++ src/authentication/restricted-area.tsx
@@ -1,36 +1,6 @@
 import * as React from 'react';
 import { MsalContext } from "msal-react"
-import {  Button } from 'antd';
-
-export function RestrictedArea({
-  children,
-  renderUnauthenticated,
-}: {
-  children: React.ReactElement;
-  renderUnauthenticated?: React.ReactElement;
-}) {
-  const { isLoggedIn } = React.useContext(MsalContext);
-  const [loggedIn, setLoggedIn] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
-  React.useEffect(() => {
-    setLoading(true);
-    isLoggedIn(['openid', 'profile'])
-      .then(v => {
-        setLoggedIn(v);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-  if (loading) {
-    return <div>loading</div>;
-  }
-  if (loggedIn) {
-    return children;
-  }
-
-  return renderUnauthenticated || null;
-}
+import { Button } from 'antd';
 
 export function LoginView() {
   const { login } = React.useContext(MsalContext);
