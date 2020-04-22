356509a181956db09c006deab02f7d1cb2a5c0d3
Jannik Buschke <jannik.buschke@gmail.com>
2020-04-22 17:16:22 +0200

    feat (featureFlags): add ifUndefined: 'show'|'hide' feature and allow 'false' value

--- src/global-settings/feature-flags.tsx
+++ src/global-settings/feature-flags.tsx
@@ -4,8 +4,17 @@ import { useGlobalSettings } from "./index"
 export function useFeatureFlags() {
   const ctx = useGlobalSettings()
 
-  function isEnabled(feature: string) {
-    return ctx[feature] == "true"
+  function isEnabled(feature: string, ifUndefined?: "show" | "hide") {
+    if (ctx[feature] == "true") {
+      return true
+    }
+    if (ctx[feature] == "false") {
+      return false
+    }
+    if (ctx[feature] === undefined || ctx[feature] === null) {
+      return ifUndefined === "show" ? true : false
+    }
+    return false
   }
 
   return isEnabled
@@ -13,12 +22,17 @@ export function useFeatureFlags() {
 
 interface Props {
   feature: string
+  ifUndefined?: "show" | "hide"
 }
 
-export const IfFeature: React.FC<Props> = ({ feature, children }) => {
+export const IfFeature: React.FC<Props> = ({
+  ifUndefined,
+  feature,
+  children,
+}) => {
   const isEnabled = useFeatureFlags()
 
-  if (isEnabled(feature)) {
+  if (isEnabled(feature, ifUndefined)) {
     return children as React.ReactElement<any>
   }
   return null
