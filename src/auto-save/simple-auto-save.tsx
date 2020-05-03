fd90b6c8980f1cd9a0909be3236e44459bc40568
Jannik Buschke <jannik.buschke@gmail.com>
2020-05-03 21:33:11 +0200

    feat (SimpleAutoSave): add getId callback

--- src/auto-save/simple-auto-save.tsx
+++ src/auto-save/simple-auto-save.tsx
@@ -2,14 +2,26 @@ import * as React from "react"
 import { useFormikContext } from "formik"
 import { useDebounce } from "use-lodash-debounce"
 
-export function SimpleAutoSave({ delayMs = 500 }: { delayMs?: number }) {
+export function SimpleAutoSave({
+  delayMs = 500,
+  getId,
+}: {
+  delayMs?: number
+  getId?: (values: any) => string
+}) {
   const ctx = useFormikContext()
   const values = useDebounce(ctx.values, delayMs)
 
   const firstUpdate = React.useRef(true)
 
   React.useLayoutEffect(() => {
-    if (!(values as any).id) {
+    if (values === null || values === undefined) {
+      console.warn("values undefined")
+      return
+    }
+    const id = getId ? getId(values) : (values as any).id
+    if (!id) {
+      console.warn("no identifier found")
       return
     }
     if (firstUpdate.current) {
